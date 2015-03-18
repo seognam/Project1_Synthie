138e9c5a9f911841d28a83dd47a0f4df0d96c42c
packerbacker89 <john3257@msu.edu>
2015-03-17 22:08:25 -0400

    Fixed waveplayer to not break on out-of-bounds call

--- Synthie/WavePlayer.cpp
+++ Synthie/WavePlayer.cpp
@@ -20,8 +20,9 @@ bool CWavePlayer::Generate()
 {
 	if (m_position < m_numsamples)
 	{
-		m_frame[0] = m_samples[m_position++] / 32768.0;
+		m_frame[0] = *(m_samples+m_position) / 32768.0;
 		m_frame[1] = m_frame[0];
+		m_position++;
 		return true;
 	}
 	else
